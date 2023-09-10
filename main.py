8eee81951fae2edf717341b9c0e8b89e598b0abe
Indi77erence <rosinov1995@mail.ru>
2023-09-10 19:06:22 +0300

    probe

--- main.py
+++ main.py
@@ -17,8 +17,6 @@ async def get_page_data(session, market):
     async with session.get(url=market) as response:
         response_text = await response.text()
         soup = BeautifulSoup(response_text, 'lxml')
-
-
         if market == 'https://burger-king.by/coupons/':
             market_name = ('BK',)
 
