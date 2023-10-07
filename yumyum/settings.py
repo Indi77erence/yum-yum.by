05e270600c309ff6be8db064e87f71117a9c9275
Indi77erence <rosinov1995@mail.ru>
2023-10-07 22:11:04 +0300

    priv_secret_key

--- yumyum/settings.py
+++ yumyum/settings.py
@@ -11,7 +11,9 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 import os.path
 from pathlib import Path
+from dotenv import load_dotenv, find_dotenv
 
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,7 +22,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-hh^ivqa8++tyampbd^5d=ycx=z&3iet$howc1%ta_pg$z@zpb('
+load_dotenv(find_dotenv())
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
